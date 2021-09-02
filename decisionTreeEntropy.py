29ec28601061e9b1b47ddbe9353d5a8daf14afc8
angela.oliveira <angela.oliveira@assert.ifpb.edu.br>
2021-09-02 10:24:01 -0300

    ajustes entropy

--- decisionTreeEntropy.py
+++ decisionTreeEntropy.py
@@ -1,20 +1,23 @@
 from sklearn import tree
-from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn.model_selection import train_test_split
 import pandas as pds
 from sklearn.neighbors import KNeighborsClassifier
 
 def Entropy():
 
-    base = "./bases/car.data"
+    # base = './bases/' + database + '.data'
+    base = './bases/balance-scale.data'
 
     dataset = pds.read_csv(base, header=None)
     
-    columns = len(dataset.columns)
-    
-    X = dataset.loc[:, 1:columns-1]
-    Y = dataset[0]
 
+    index_Y = 0
+    index_inicial = 1
+    index_final = len(dataset)
+
+    Y = dataset[index_Y]
+    X = dataset.loc[:,index_inicial:index_final]
 
     # 20% teste e 80% treinamento
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=None, stratify=Y)
@@ -29,6 +32,7 @@ def Entropy():
     result_final = metrics.accuracy_score(resultado, Y_test)
 
     final = round(result_final * 100)
+    print('Base balance-scale')
 
     print("{}%".format(final))
     
